e7b6cc3956c2517a95dbe960c4b81a083f1ffebe
sclfnc994 <sclfnc@proton.me>
2025-12-10 12:52:15 +0100

    Updatet test

--- examples/RunStochModel(TBD).jl
+++ examples/RunStochModel(TBD).jl
@@ -45,6 +45,7 @@ output_file_combined = "outputs/output_file_SCO.xlsx"  # Output file - model Ene
 output_plot_ren = "outputs/Img/plot_stoch_renewable_assets.png"  # Output png file of plot - renewable assets
 output_plot_batt = "outputs/Img/plot_stoch_battery_assets.pdf"  # Output png file of plot - battery assets
 
+mkpath("docs/src/examples/outputs/Img")  # create output folder if it does not exist
 # Generate the set of scenarios used to optimize the EC
 scenarios = build_scenarios(input_file)
 
@@ -62,7 +63,7 @@ build_specific_model!(GroupCO(),ECModel_SCO, HiGHS.Optimizer)
 optimize_deterministic_ECmodel(ECModel_SCO)
 
 # print summary
-print_summary(ECModel_SCO)
+# print_summary(ECModel_SCO)
 
 # Save results
 print_stochastic_results(output_file_combined, ECModel_SCO)
@@ -73,25 +74,29 @@ print_stochastic_results(output_file_combined, ECModel_SCO)
 ECModel_SNC = StochasticEC(input_file, EnergyCommunity.GroupNC(), scenarios, optimizer = HiGHS.Optimizer)
 
 # build SNC model
-build_base_model!(GroupNC(),ECModel_SNC, HiGHS.Optimizer)
+build_base_model!(ECModel_SNC, HiGHS.Optimizer)
 
 # optimize SNC model
 optimize_deterministic_ECmodel(ECModel_SNC)
 
 # print summary of SNC model
-print_summary(ECModel_SNC)
+# print_summary(ECModel_SNC)
 
 # Save results
 print_stochastic_results(output_file_isolated, ECModel_SNC)
 
 # Plot some useful image of the installed capacity
-colors = wong_colors()
+# colors = Makie.wong_colors()
+colors = palette(:default)
 
 # Evaluate the installed capacity of the entire EC - model Energy community
 x_tot_SCO = calculate_x_tot(EC_Cooperative)
 
 # Evaluate the installed capacity of the entire EC - model users alone
 x_tot_SNC = calculate_x_tot(EC_NonCooperative)
+
+# [:red, :blue]
+# [:green]
 
 # Plot installed renewable assets in both SCO and SNC versions
 plot_resource(output_plot_ren, users_data, ["PV","wind"], x_tot_SCO, x_tot_SNC, colors[1:2]) # renewable asset
